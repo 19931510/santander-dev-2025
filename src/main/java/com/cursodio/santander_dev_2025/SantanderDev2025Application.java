10b75fdc7c910439f5c8e7df3df872e492056f7a
19931510 <caroline.arena9333@gmail.com>
2025-03-25 18:36:13 -0300

    Initial commit

---
+++ src/main/java/com/cursodio/santander_dev_2025/SantanderDev2025Application.java
@@ -0,0 +1,13 @@
+package com.cursodio.santander_dev_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDev2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDev2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursodio/santander_dev_2025/SantanderDev2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodio.santander_dev_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDev2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
